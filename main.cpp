f8ec77d09278dec4ac8a258dc7fca92eca721445
jhxqy <1139648416@qq.com>
2019-08-28 22:09:14 +0800

    关于闭包的构成和匹配可能存在问题

--- main.cpp
+++ main.cpp
@@ -17,6 +17,21 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     NFA a("hello");
     NFA b("nice!");
-    cout<<NFAOperator::Closure(a).match("hellohello")<<endl;
+    NFA c=NFAOperator::Cnt(NFAOperator::Closure(NFA("aaa")), NFA("abb"));
+    NFA d=NFAOperator::Closure(NFA("ddd"));
+    NFA f=NFAOperator::Closure(NFAOperator::Or(NFA("aa"), NFA("bb")));
+    cout<<f.match("aa")<<endl;
+    cout<<f.match("aabb")<<endl;
+    cout<<f.match("aaaa")<<endl;
+    cout<<f.match("bbbb")<<endl;
+
+    cout<<d.match("ddd")<<endl;
+    cout<<d.match("dddd")<<endl;
+    cout<<d.match("ddddd")<<endl;
+    cout<<d.match("dddddd")<<endl;
+
+    cout<<c.match("aaaabb")<<endl;
+    cout<<c.match("abababb")<<endl;
+    cout<<c.match("aaaab")<<endl;
     return 0;
 }
